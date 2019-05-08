b4abed0bb02a1dd92ab7ee600f9b20add1c42500
carulasan <cycarulasan@gmail.com>
2019-05-08 17:09:37 +0800

    'fasdf'

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$4sqd#ua@hmi-%7!y0!rr#-zigdbitp(pxf966j8o&b7%ysh#_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
